23f0439fa0cf4801e36240855a3c6af12b844172
Dmitri9149 <dmitri.as.name@gmail.com>
2019-12-23 15:59:50 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { useState } from 'react'
 
+import {
+  BrowserRouter as Router,
+  Route, Link, Redirect, withRouter
+} from 'react-router-dom'
+
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
   }
+
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Router>
+        <div>
+          <div>
+            <Link style={padding} to="/">anecdotes</Link>
+            <Link style={padding} to="/create">create new</Link>
+            <Link style={padding} to="/about"></Link>
+          </div>
+          <Route exact path="/" render={() => <AnecdoteList />} />
+          <Route path="/create" render={() => <CreateNew />} />
+          <Route path="/about" render={() => <About />} />
+        </div>
+      </Router>
     </div>
   )
 }
